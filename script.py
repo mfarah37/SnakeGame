d48b6d9b1ac1356fabbb2934eb68f7542c775f0a
Mohamed <mfarah37@gmail.com>
2022-05-13 21:00:31 -0700

    first commit

---
+++ script.py
@@ -0,0 +1,71 @@
+import pygame 
+import sys
+import random
+
+class snake(object):
+    def __init__(self):
+        pass
+    def get_head_position(self):
+        pass
+    def turn(self, point):
+        pass
+    def move(self):
+        pass
+    def reset(self):
+        pass
+    def draw(self, surface):
+        pass
+    def handle_keys(self):
+        pass
+
+class food(object):
+    def __init__(self):
+        pass
+    def randomize_position(self):
+        pass
+    def draw(self, surface):
+        pass
+
+def drawGrid(surface):
+    for y in range(0, int(GRID_HEIGHT)):
+        for x in range(0, int(GRID_WIDTH)):
+            if (x + y) % 2 == 0:
+                r = pygame.Rect((x*GRIDSIZE, y*GRIDSIZE), (GRIDSIZE, GRIDSIZE))
+                pygame.draw.rect(surface, (93, 216, 228), r)
+            else:
+                rr = pygame.Rect((x*GRIDSIZE, y*GRIDSIZE), (GRIDSIZE, GRIDSIZE))
+                pygame.draw.rect(surface, (84, 194, 205), rr)
+
+SCREEN_WIDTH = 480
+SCREEN_HEIGHT = 480
+
+GRIDSIZE = 20
+GRID_WIDTH = SCREEN_HEIGHT / GRIDSIZE
+GRID_HEIGHT = SCREEN_WIDTH / GRIDSIZE
+
+UP = (0, -1)
+DOWN = (0, 1)
+LEFT = (-1, 0)
+RIGHT = (1, 0)
+
+def main ():
+    pygame.init()
+
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
+
+    surface = pygame.Surface(screen.get_size())
+    surface = surface.convert()
+    drawGrid(surface)
+
+    snake = snake()
+    food = food()
+
+    score = 0
+    while(True):
+        clock.tick(10)
+        # handle events 
+        screen.blit(surface, (0, 0))
+        pygame.display.update()
+
+main()
